1ce3dae5391e35c3d9560513ab9117faae604ef9
TGMK3 <55986637+TGMK3@users.noreply.github.com>
2021-02-02 16:25:19 +0800

    Update hh.java

--- src/main/java/hh.java
+++ src/main/java/hh.java
@@ -2,5 +2,7 @@ public class hh {
     public static void main(String[] args) {
         System.out.println("TG");
         System.out.println("TG01");
+        System.out.println("TG02");
+        
     }
 }
